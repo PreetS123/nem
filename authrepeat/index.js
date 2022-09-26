a5d8a9d43b5c9aef354bab17e6be1cc24310a3e5
PreetS12301~ <preetbhardwaj517@gmail.com>
2022-09-26 16:34:00 +0530

    http-request

---
+++ authrepeat/index.js
@@ -0,0 +1,80 @@
+
+ const express= require('express');
+  require('dotenv').config();
+  const {connection}= require('./config/db');
+   const UserModel= require('./models/User.model')
+  const app= express();
+  const jwt= require('jsonwebtoken');
+  
+
+  app.use(express.json());
+
+
+     app.get('/',(req,res)=>{
+        return res.send(`<a href='#'>Login via Github</a>`)
+     })
+     app.get('/about',(req,res)=>{
+         return res.send('ABOUT PAGE');
+     })
+    
+  app.post ('/signup',(req,res)=>{
+    const {email,password,age}= req.body;
+    const user= new UserModel({
+        email,
+        password,
+        age
+    })
+    user.save();
+    res.send('SignUp Successfull');
+  })
+
+  app.post('/login', async(req,res)=>{
+    const {email,password}= req.body;
+      
+    const user= await UserModel.find({email,password});
+
+    const token=jwt.sign({email:user.email,age:user.age,_id:user._id},'secret')
+
+      if(user.length===0){
+        return res.send('Invalid Credentials');
+      }
+      return res.send({message:'Login Successful',token:token});
+  })
+
+    app.get('/profile/:id',async(req,res)=>{
+        const id= req.params.id;
+        // const user_token= req.query.token;
+        const user_token= req.headers.authorization.split(" ")[1]
+         
+    //    if(token!=12345){
+    //     return res.send('Invalid User')
+    //    }
+
+        // verification of token
+        jwt.verify(user_token,'secret',function(err,decoded){
+            if(err){
+                return res.send('Please Login Again!')
+            }
+            console.log(decoded);
+        });
+        try
+        {
+        const user= await UserModel.find({_id:id},{password:0,__v:0})
+        return res.send(user);
+        }
+        catch
+        {
+            return res.send('No Such Data Found');
+        }
+    })
+
+  app.listen(process.env.PORT,async()=>{
+    try{
+        await connection;
+        console.log('connection established')
+    }
+    catch{
+        console.log('error in atlas')
+    }
+    console.log(`Listening on ${process.env.PORT}`);
+  })
---
+++ http/index.js
@@ -0,0 +1,60 @@
+
+  const http= require('http');
+
+
+
+  const server= http.createServer((req,res)=>{
+    console.log(req.method)
+    if(req.url==='/post' ) {
+        let arrp=[
+            {
+            "postId": 1,
+            "id": 1,
+            "name": "alias odio sit",
+            "email": "Lew@alysha.tv",
+            "body": "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati"
+          },
+          {
+            "postId": 1,
+            "id": 2,
+            "name": "vero eaque aliquid doloribus et culpa",
+            "email": "Hayden@althea.biz",
+            "body": "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et"
+          }
+        ]
+        res.end(JSON.stringify(arrp));
+    }
+    else if(req.url==='/comments'){
+     let arr=[
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "qui est esse",
+          "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        }
+    ]
+    res.end(JSON.stringify(arr))
+    }
+    else if(req.url==='/march'){
+        let obj={
+            no_of_ref:54,
+            suc_ref:34,
+            perc:'80%',
+        }
+        res.end(JSON.stringify(obj))
+    }
+    else{
+         res.end('Welcome to homepage')
+    }
+  })
+
+
+  server.listen(8000,()=>{
+    console.log('Listening on 8000')
+  })
